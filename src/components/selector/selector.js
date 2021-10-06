7b86630b46ccfbc56456780dcd758d8d7bbb22af
DennisMüller <dm499555@.gmail.com>
2021-10-06 12:16:38 +0200

    first articles

---
+++ src/components/selector/selector.js
@@ -0,0 +1,9 @@
+import "./favorite-card.css";
+import favoriteCard from "./selector.html";
+
+export default {
+  title: "Components/selector",
+  parameters: { layout: "centered" },
+};
+
+export const fruits = () => favoriteCard;
